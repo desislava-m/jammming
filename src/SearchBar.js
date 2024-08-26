1795912da77378e8fb9685fc069e45ae38307358
Dessie <desislava.m.alexandrova@gmail.com>
2024-08-26 15:08:44 +0300

    added no results found page

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -30,23 +30,25 @@ export default function SearchBar() {
 
     const handleSubmit = (event) => {
         event.preventDefault()
-        
+
         if (searchBar.length > 0) {
             const results = songs.filter((song) => {
                 return song.title.toLowerCase().match(searchBar.toLowerCase()) ||
                     song.artist.toLowerCase().match(searchBar.toLowerCase());
             })
-
-            setFilteredSongs(results)
-        } else if (searchBar.length === '') {
+            if(results.length === 0) {
+                setFilteredSongs('No results found')
+            } else {
+                setFilteredSongs(results)
+            }
+            
+        } else  {
             setFilteredSongs(songs)
-        } else {
-            setFilteredSongs([]);
-        };
+        } 
     }
 
     const addToPlaylist = (song) => {
-        const songExists = playlist.some(playlistSong => 
+        const songExists = playlist.some(playlistSong =>
             playlistSong.title === song.title && playlistSong.artist === song.artist
         );
         if (!songExists) {
@@ -72,18 +74,23 @@ export default function SearchBar() {
             <div className={styles.glassContainer}>
                 <h1 className={styles.h1}>Jammming</h1>
                 <div className={styles.searchBarForm}>
-                <form onSubmit={handleSubmit}>
-                    <input type="text" value={searchBar} onChange={handleChange} placeholder='Search by title or artist'></input>
-                    <button type='submit' className={styles.button}>Search</button>
-                </form>
+                    <form onSubmit={handleSubmit}>
+                        <input type="text" value={searchBar} onChange={handleChange} placeholder='Search by title or artist'></input>
+                        <button type='submit' className={styles.button}>Search</button>
+                    </form>
                 </div>
                 <div className={styles.resultsAndPlaylist}>
                     <div>
-                    {filteredSongs.length > 0 ? (<SearchResults songs={filteredSongs} onAdd={addToPlaylist} />) : 
-                    (<h2 className={style.h2}>No results found :(</h2>)}
+                        {filteredSongs === 'No results found' ? (
+                            <h2 className={style.h2}>No results found :(</h2>
+                        ) : (
+                            filteredSongs.length > 0 && (
+                                <SearchResults songs={filteredSongs} onAdd={addToPlaylist} />
+                            )
+                        )}
                     </div>
                     <div>
-                    {playlist.length > 0 && <Playlist playlist={playlist} playlistName={playlistName} onRemove={removeSong} onNameChange={handleNameChange} />}
+                        {playlist.length > 0 && <Playlist playlist={playlist} playlistName={playlistName} onRemove={removeSong} onNameChange={handleNameChange} />}
                     </div>
                 </div>
             </div>
